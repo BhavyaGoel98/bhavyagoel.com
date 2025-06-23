c87e29f87cadf5d607ef646cbecbb09a9cf278ec
BhavyaGoel98 <bgoeldefender@gmail.com>
2025-06-23 13:47:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+a {
+  text-decoration: none;        /* removes underline */
+  background-color: red;        /* red background */
+  color: white;                 /* white text */
+  padding: 6px 12px;            /* space around the link text */
+  border-radius: 6px;           /* rounded corners */
+  display: inline-block;        /
